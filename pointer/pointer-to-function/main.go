d038353d4b63b64d8f3957810556ed7b0a6363cf
Azamat Bekmuratov <azamat.bekmuratov@gmail.com>
2022-05-20 14:23:50 +0600

    point to function implementation

---
+++ pointer/pointer-to-function/main.go
@@ -0,0 +1,31 @@
+// Go program to create a pointer
+// and passing it to the function
+package main
+
+import "fmt"
+
+// taking a function with integer
+// type pointer as an parameter
+func ptf(a *int) {
+	//dereferencing
+	*a = 120
+}
+
+func main() {
+
+	// taking a normal variable
+	var x = 100
+
+	fmt.Printf("The value of x before function call is: %d\n", x)
+
+	// taking a pointer variable
+	// and assigning the address
+	// of x to it
+	var pa *int = &x
+
+	// calling the function by
+	// passing pointer to function
+	ptf(pa)
+
+	fmt.Printf("The value of x after function call is: %d\n", x)
+}
