aa59c73c58592955a3e8c6c798349828eaaca7d7
Azamat Bekmuratov <azamat.bekmuratov@gmail.com>
2022-06-23 08:59:33 +0600

    solid principle implementation

---
+++ solid/open-closed-principle/main.go
@@ -0,0 +1,30 @@
+// open for extension, closed for modification
+package main
+
+import "fmt"
+
+type Cat struct {
+	Name string
+}
+
+func (c Cat) Legs() int {
+	return 4
+}
+
+func (c Cat) PrintLegs() {
+	fmt.Printf("I have %d legs\n", c.Legs())
+}
+
+type OctoCat struct {
+	Cat
+}
+
+func (oc OctoCat) Legs() int {
+	return 5
+}
+
+func main() {
+	var octo OctoCat
+	fmt.Println(octo.Legs()) // 5
+	octo.PrintLegs()         // I have 4 legs
+}
